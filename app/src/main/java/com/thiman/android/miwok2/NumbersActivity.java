f63c33925bb21ebbef42096e0998f6de4e3aaf23
Thimankrishh <mysoftware15@gmail.com>
2017-11-25 13:49:31 +0530

    Array List

--- app/src/main/java/com/thiman/android/miwok2/NumbersActivity.java
+++ app/src/main/java/com/thiman/android/miwok2/NumbersActivity.java
@@ -6,6 +6,8 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Toast;
 
+import java.util.ArrayList;
+
 public class NumbersActivity extends AppCompatActivity {
 
     @Override
@@ -13,19 +15,19 @@ public class NumbersActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_numbers);
 
-        String[] words = new String[10];
-        words[0] = "One";
-        words[1] = "Two";
-        words[2] = "Three";
-        words[3] = "Four";
-        words[4] = "Five";
-        words[5] = "Six";
-        words[6] = "Seven";
-        words[7] = "Eight";
-        words[8] = "Nine";
-        words[9] = "Ten";
+        ArrayList<String> words = new ArrayList<String>();
+        words.add("One");
+        words.add("Two");
+        words.add("Three");
+        words.add("Four");
+        words.add("Five");
+        words.add("Six");
+        words.add("Seven");
+        words.add("Eight");
+        words.add("Nine");
+        words.add("Ten");
 
-        Log.v("NumbersActivity","word at index 0: " + words[0]);
+        Log.v("NumbersActivity","word at index 0: " + words.get(0));
     }
 }
 
